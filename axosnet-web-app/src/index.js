ce6003d2cd2ace2c22c8f9fdc00953e70f7b2f5c
Enrique Tolentino <enrique.tole@outlook.com>
2020-03-22 20:50:05 -0600

    All receipts

--- axosnet-web-app/src/index.js
+++ axosnet-web-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bulma/css/bulma.css';
+import './css/Spacing.css';
 import './css/App.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
--- axosnet-web-app/src/views/receipts/Index.js
+++ axosnet-web-app/src/views/receipts/Index.js
@@ -1,34 +1,75 @@
 import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import ReceiptsService from '../../services/ReceiptsService';
+import { formatDate, formatNumber, convertUTCDateToLocalDate } from '../../helpers/Formats';
 
 const initialState = {
 	receipts: []
 };
 
 export class ReceiptsIndex extends Component {
-    state = initialState;
+	state = initialState;
 
-    receiptsService = new ReceiptsService();
+	receiptsService = new ReceiptsService();
 
-    async componentDidMount() {
+	async componentDidMount() {
 		this.setState({
 			receipts: await this.receiptsService.GetAllReceipts()
 		});
 	}
 
 	render() {
-        const {receipts} = this.state;
-        console.log(receipts);
+		const { receipts } = this.state;
 		return (
 			<Fragment>
 				<div className="section">
 					<div className="card container">
 						<div className="card-content">
 							<h3 className="title">My Receipts</h3>
-							<Link to="/receipts/create" className="button is-link">
+							<Link to="/receipts/create" className="button is-primary">
 								<i className="fas fa-plus fa-fw" />New Receipt
 							</Link>
+							<div className="table-container m-t-md">
+								<table className="table is-fullwidth is-hoverable is-striped">
+									<thead>
+										<tr className="is-selected has-background-link">
+											<th>ID</th>
+											<th>Date</th>
+											<th>Provider</th>
+											<th>Amount</th>
+											<th>Currency</th>
+											<th />
+										</tr>
+									</thead>
+									<tbody>
+										{receipts.map((receipt, index) => (
+											<tr key={index}>
+												<td>{receipt.idReceipt}</td>
+												<td>
+													{formatDate(
+														convertUTCDateToLocalDate(receipt.date),
+														'MMMM dd, yyyy'
+													)}
+												</td>
+												<td>{receipt.providerCode}</td>
+												<td>{formatNumber.new(receipt.amount, '$')}</td>
+												<td>{receipt.currency.code}</td>
+												<td>
+													<Link
+														to={'/receipts/edit/' + receipt.idReceipt}
+														className="button is-small is-primary m-r-sm"
+													>
+														<i class="fas fa-edit fa-fw" />Edit
+													</Link>
+													<Link to="/receipts/create" className="button is-small is-danger">
+														<i class="fas fa-trash-alt fa-fw" />Delete
+													</Link>
+												</td>
+											</tr>
+										))}
+									</tbody>
+								</table>
+							</div>
 						</div>
 					</div>
 				</div>
